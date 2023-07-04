4d823ad7b31d9ec51078837c7eb36a728862da06
Yasin Cakal <whoisyasin@gmail.com>
2023-07-04 21:51:32 +0300

    deleted testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom'
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import {
+  About,
+  Contact,
+  Experience,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from './components'
 
 const App = () => {
   return (
@@ -14,14 +23,13 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,12 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import Hero from "./Hero";
-import Navbar from "./Navbar";
-import About from "./About";
-import Tech from "./Tech";
-import Experience from "./Experience";
-import Works from "./Works";
-import Feedbacks from "./Feedbacks";
-import Contact from "./Contact";
-import CanvasLoader from "./Loader";
-
+import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas'
+import Hero from './Hero'
+import Navbar from './Navbar'
+import About from './About'
+import Tech from './Tech'
+import Experience from './Experience'
+import Works from './Works'
+import Contact from './Contact'
+import CanvasLoader from './Loader'
 
 export {
   Hero,
@@ -17,11 +15,10 @@ export {
   Tech,
   Experience,
   Works,
-  Feedbacks,
   Contact,
   CanvasLoader,
-  EarthCanvas, 
-  BallCanvas, 
-  ComputersCanvas, 
-  StarsCanvas
-};
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
+  StarsCanvas,
+}
