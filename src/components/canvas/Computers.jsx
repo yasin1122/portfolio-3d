53b18f97e605be70f0e7e3b45e907e6e4fa082e2
Yasin Cakal <whoisyasin@gmail.com>
2025-04-05 12:23:38 -0700

    testing new scroll

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,15 +1,18 @@
-import React, { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import React, { Suspense, useEffect, useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
-import CanvasLoader from "../Loader";
+import CanvasLoader from '../Loader'
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF('./desktop_pc/scene.gltf')
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight
+        intensity={0.15}
+        groundColor='black'
+      />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -26,32 +29,25 @@ const Computers = ({ isMobile }) => {
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
-  );
-};
+  )
+}
 
-const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false);
+/**
+ * domElement (optional) – HTMLElement whose pointer events will control
+ * OrbitControls. When undefined, OrbitControls listens on the whole canvas.
+ */
+const ComputersCanvas = ({ domElement }) => {
+  const [isMobile, setIsMobile] = useState(false)
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+    setIsMobile(mediaQuery.matches)
 
-    // Set the initial value of the `isMobile` state variable
-    setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle changes to the media query
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
-    };
-
-    // Add the callback function as a listener for changes to the media query
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
-  }, []);
+    const handleMediaQueryChange = event => setIsMobile(event.matches)
+    mediaQuery.addEventListener('change', handleMediaQueryChange)
+    return () =>
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+  }, [])
 
   return (
     <Canvas
@@ -59,20 +55,20 @@ const ComputersCanvas = () => {
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
-      gl={{ preserveDrawingBuffer: true }}
-    >
+      gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
+          domElement={domElement}
         />
         <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
     </Canvas>
-  );
-};
+  )
+}
 
-export default ComputersCanvas;
+export default ComputersCanvas
